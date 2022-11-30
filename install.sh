5543d3d2d741e26068f0e4277d06399f2e4ef216
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-11-30 11:00:33 +0100

    Fix install.sh

--- install.sh
+++ install.sh
@@ -17,20 +17,14 @@ rm -rf /usr/share/plasma/desktoptheme/EupneaOS-dark
 rm -rf /usr/share/plasma/desktoptheme/EupneaOS-light
 rm -rf /usr/share/plasma/look-and-feel/com.github.eupnea-linux.EupneaOS-dark
 rm -rf /usr/share/plasma/look-and-feel/com.github.eupnea-linux.EupneaOS-light
+rm -rf /usr/share/plasma/layout-templates/org.github.eupnea-linux.EupneaOSPanel
 rm -rf /usr/share/color-schemes/EupneaOSDark.colors
 rm -rf /usr/share/color-schemes/EupneaOSLight.colors
 rm -rf /usr/share/Kvantum/EupneaOS-dark
 rm -rf /usr/share/Kvantum/EupneaOS-light
 rm -rf /usr/share/wallpapers/EupneaOS-dark
 rm -rf /usr/share/wallpapers/EupneaOS-light
-rm -rf /usr/share/sddm/themes/EupneaOS-sddm
+rm -rf /usr/share/sddm/themes/EupneaOS
 
 echo "Copying new files..."
-cp -r aurorae /usr/share/aurorae/themes
-cp -r color-schemes /usr/share/color-schemes
-cp -r Kvantum /usr/share/Kvantum
-cp -r plasma/desktoptheme /usr/share/plasma/desktoptheme
-cp -r plasma/layout-templates /usr/share/plasma/layout-templates
-cp -r plasma/look-and-feel /usr/share/plasma/look-and-feel
-cp -r wallpaper /usr/share/wallpapers
-cp -r sddm /usr/share/sddm/themes/EupneaOS-sddm
+cp -r aurorae color-schemes Kvantum plasma wallpaper sddm /usr/share/
